f085aba16be7e45aa085fa6a9d0d57cbe1a41097
Sean Watters <seanwatters11@gmail.com>
2022-12-16 16:34:40 +0000

    creative tab

--- src/main/java/me/krynox/elderthings/common/CreativeTab.java
+++ src/main/java/me/krynox/elderthings/common/CreativeTab.java
@@ -2,32 +2,30 @@ package me.krynox.elderthings.common;
 
 import me.krynox.elderthings.ElderThings;
 import me.krynox.elderthings.client.localisation.LocalisationHelper;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraftforge.event.CreativeModeTabEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.ObjectHolder;
 
 import static me.krynox.elderthings.client.localisation.LocalisedTextCategory.CREATIVE_TAB;
 import static me.krynox.elderthings.setup.Registration.EXAMPLE_BLOCK_ITEM;
 
-@Mod.EventBusSubscriber(modid = ElderThings.MODID, bus = Mod.EventBusSubscriber.Bus.FORGE)
+@Mod.EventBusSubscriber(modid = ElderThings.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class CreativeTab {
     @SubscribeEvent
     public static void registerCreativeTab(CreativeModeTabEvent.Register e) {
         //todo - creative tab isn't appearing, maybe this event handler is improperly registered, maybe it's just not doing the right thing
-        ElderThings.LOGGER.debug("ElderThings handling CreativeModeTabEvent.Register");
 
-        e.registerCreativeModeTab(ElderThings.newResourceLocation("elderthings_creativetab"), builder -> builder
+        e.registerCreativeModeTab(ElderThings.newResourceLocation("creativetab"), builder -> builder
                 .icon(() -> new ItemStack(Items.ACACIA_BOAT))
-                .title(LocalisationHelper.newUnlocName(CREATIVE_TAB, "elderthings_tab")));
+                .title(LocalisationHelper.newUnlocName(CREATIVE_TAB, "creativetab"))
+                .displayItems((featureFlags, output, hasOp) -> {
+                    output.accept(ModItems.EXAMPLE_BLOCK);
+                }));
     }
 
-    @SubscribeEvent
-    public static void populateCreativeTab(CreativeModeTabEvent.BuildContents e) {
-        if (e.getTab() == CreativeModeTabs.BUILDING_BLOCKS)
-            e.accept(EXAMPLE_BLOCK_ITEM);
-
-    }
 }
---
+++ src/main/java/me/krynox/elderthings/common/ModItems.java
@@ -0,0 +1,11 @@
+package me.krynox.elderthings.common;
+
+import me.krynox.elderthings.ElderThings;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.registries.ObjectHolder;
+
+public class ModItems {
+
+    @ObjectHolder(registryName = "minecraft:item", value = "elderthings:example_block")
+    public static final Item EXAMPLE_BLOCK = null;
+}
